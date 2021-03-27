7e42686df385d751b156c3bd2a75e1d8511b0269
Jupchurch2 <81187233+Jupchurch2@users.noreply.github.com>
2021-03-27 15:41:54 -0400

    Update AppModel.py

---
+++ AirplaneSeating/AppModel.py
@@ -0,0 +1,15 @@
+from Plane import Seat
+from Passenger import Passenger
+
+
+class AppModel:
+   
+   def __init__(self):
+      self.seatList = []
+      
+   def populateSeatList(self):
+      for i in range(120):
+         
+      
+   
+      
