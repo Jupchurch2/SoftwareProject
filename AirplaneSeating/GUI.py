1eae6a0edc02415b8f81116bd0860b229a3f012d
babelr1 <babelj16@gmail.com>
2021-03-27 15:48:13 -0400

    Edited GUI attributes cleaned up the code

--- AirplaneSeating/GUI.py
+++ AirplaneSeating/GUI.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter import simpledialog
 
 # -----------------------------------------------------------------------------------------------------------------
 
@@ -7,12 +6,22 @@ managerWindow = Tk()
 login = Toplevel()
 passengerWindow = Toplevel()
 mP = Toplevel()
+reportScreen = Toplevel()
+
+# -----------------------------------------------------------------------------------------------------------------
 
 mbHEIGHT = 6
 mbWIDTH = 30
 window = '800x800'
 planeImage = PhotoImage(file='planelayout_New.png')
 managerPhrase = 'ManagerLogin'
+
+managerViewButtons = []
+passengerViewButtons = []
+seatX = 330
+seatY = 160
+
+# -----------------------------------------------------------------------------------------------------------------
 
 passengerWindow.geometry(window)
 passengerWindow.title('Guest View')
@@ -27,6 +36,10 @@ mP.title('Manager Password')
 login.geometry(window)
 login.title('Login')
 login.config(bg='grey')
+
+reportScreen.geometry(window)
+reportScreen.title('Passenger Report')
+reportScreen.config(bg='light blue')
 
 # -----------------------------------------------------------------------------------------------------------------
 
@@ -65,28 +78,41 @@ def logout():
 
 # -----------------------------------------------------------------------------------------------------------------
 
-planeLabel = Label(passengerWindow, image=planeImage)
-planeLabel.place(x=175, y=30)
+def seatButtons(master, seatList, x, y):
+    """
+    Function to create 120 seats for the plane diagram
+    :param master: str Which window the buttons are placed in
+    :param seatList: list The list where all the seats are appended to
+    :param x: int The starting X coordinate
+    :param y: int The starting Y coordinate
+    :return: None
+    """
+    for i in range(1, 120 + 1):
+        b = Button(master, text=f"{i}", bg='green', fg='white', width=2, height=1)
+        seatList.append(b)
+        b.place(x=x, y=y)
+        x += 23
+        if i % 3 == 0:
+            if i % 6 == 0:
+                x -= 161
+                y += 25
+            else:
+                x += 23
 
-passengerViewButtons = []
-x = 330
-y = 160
-
-for i in range(1, 120 + 1):
-    b = Button(passengerWindow, text=f"{i}", bg='green', fg='white', width=2, height=1)
-    passengerViewButtons.append(b)
-    b.place(x=x, y=y)
-    x += 23
-    if i % 3 == 0:
-        if i % 6 == 0:
-            x -= 161
-            y += 25
-        else:
-            x += 23
 
 # -----------------------------------------------------------------------------------------------------------------
 
-mP.bind('<Return>', loginButton)
+# Creates the plane image and all 120 seat buttons in both windows
+planeLabelPassenger = Label(passengerWindow, image=planeImage).place(x=175, y=30)
+seatButtons(passengerWindow, passengerViewButtons, seatX, seatY)
+
+planeLabelManager = Label(managerWindow, image=planeImage).place(x=175, y=30)
+seatButtons(managerWindow, managerViewButtons, seatX, seatY)
+
+# -----------------------------------------------------------------------------------------------------------------
+
+reportButton = Button(managerWindow, text='Print Report', bg='blue', fg='white')
+reportButton.place(x=5, y=770)
 
 button = Button(login, text="Passenger", height=mbHEIGHT, width=mbWIDTH, command=lambda: passengerLogin())
 button.place(relx=.363, rely=.2)
@@ -105,8 +131,13 @@ logoutButton.place(x=740, y=770)
 logoutButton2 = Button(passengerWindow, text='Exit', width=6, fg='red', command=lambda: logout())
 logoutButton2.place(x=740, y=770)
 
-# Creating an event loop
+# -----------------------------------------------------------------------------------------------------------------
+
+# Withdrawing all windows we do not want to see immediately
+reportScreen.withdraw()
 managerWindow.withdraw()
 passengerWindow.withdraw()
 mP.withdraw()
+
+# Creating an event loop
 managerWindow.mainloop()
