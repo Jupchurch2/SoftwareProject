823eb727a19d8a46187fca06a4cf1f568e58df1a
babelr1 <babelj16@gmail.com>
2021-04-07 17:26:26 -0400

    updated buttons to grab names and create passenger objects w separate screens.

--- AirplaneSeating/GUI.py
+++ AirplaneSeating/GUI.py
@@ -1,5 +1,6 @@
 # Import all libraries to make the application functional
 from tkinter import *
+from tkinter import messagebox
 #from AppModel import *
 #from Plane import *
 #from Passenger import *
@@ -83,11 +84,15 @@ usernameBox.place(relx=.4, rely=.3)
 categoryBox = OptionMenu(categoryScreen, clicked, *categories,)
 categoryBox.place(relx=.4, rely=.3)
 
+# -----------------------------------------------------------------------------------------------------------------
+
 businessName = Entry(businessTicket, width=30)
 businessName.place(relx=.4, rely=.25)
 
 businessPref = Entry(businessTicket, width=10)
 businessPref.place(relx=.4, rely=.5)
+
+# -----------------------------------------------------------------------------------------------------------------
 
 touristName1 = Entry(touristTicket, width=30)
 touristName2 = Entry(touristTicket, width=30)
@@ -99,38 +104,71 @@ touristPref2 = Entry(touristTicket, width=10)
 touristPref1.place(x=.5, y=.5)
 touristPref2.place(x=.5, y=.7)
 
+# -----------------------------------------------------------------------------------------------------------------
+
 familyName1 = Entry(familyTicket, width=30)
 familyName2 = Entry(familyTicket, width=30)
 familyName3 = Entry(familyTicket, width=30)
 familyName4 = Entry(familyTicket, width=30)
 familyName5 = Entry(familyTicket, width=30)
-
-familyPref1 = Entry(familyTicket, width=10)
-familyPref2 = Entry(familyTicket, width=10)
-familyPref3 = Entry(familyTicket, width=10)
-familyPref4 = Entry(familyTicket, width=10)
-familyPref5 = Entry(familyTicket, width=10)
-
 familyName1.place(x=.5, y=.15)
 familyName2.place(x=.5, y=.30)
 familyName3.place(x=.5, y=.45)
 familyName4.place(x=.5, y=.60)
 familyName5.place(x=.5, y=.75)
 
+familyPref1 = Entry(familyTicket, width=10)
 familyPref1.place(x=.5, y=.2)
-familyPref2.place(x=.5, y=.35)
-familyPref3.place(x=.5, y=.5)
-familyPref4.place(x=.5, y=.65)
-familyPref5.place(x=.5, y=.8)
+
+# -----------------------------------------------------------------------------------------------------------------
+
 
 # Defining button functions
-def bookTicket():
+def bookBusinessTicket():
     fullName = businessName.get()
-    first, last = fullName.split(' ')
-    preference = businessPref.get()
-    businessTicket.withdraw()
-    # newPassenger = Passenger(first, last, category, preference)
+    if len(fullName) > 1:
+        first, last = fullName.split(' ')
+        preference = businessPref.get()
+        businessTicket.withdraw()
+        # newPassenger = Passenger(first, last, 'Business', preference)
+    else:
+        messagebox.showwarning(title='Your Name', message='Please enter your first and last name.')
 
+def bookTouristTicket():
+    fullName1 = touristName1.get()
+    fullName2 = touristName2.get()
+    if fullName1 > 1 and fullName2 > 1:
+        first1, last1 = fullName1.split(' ')
+        first2, last2 = fullName2.split(' ')
+        pref1 = touristPref1.get()
+        pref2 = touristPref2.get()
+        #passenger1 = Passenger(first1, last1, 'Tourist', pref1)
+        #passenger2 = Passenger(first2, last2, 'Tourist', pref2)
+    else:
+        messagebox.showwarning(title='Your Name', message='Please enter both of your first and last names.')
+
+def bookFamilyTicket():
+    fullname1 = familyName1.get()
+    fullname2 = familyName2.get()
+    fullname3 = familyName3.get()
+    fullname4 = familyName4.get()
+    fullname5 = familyName5.get()
+    if fullname1 > 1 and fullname2 > 1 and fullname3 > 1:
+        first1, last1 = fullname1.split(' ')
+        pref1 = familyPref1.get()
+        first2, last2 = fullname2.split(' ')
+        first3, last3 = fullname3 .split(' ')
+        if fullname4 > 1:
+            first4, last4 = fullname4.split(' ')
+            #passenger4 = Passenger(first4, last4, Family, pref1)
+        elif fullname5 > 1:
+            first5, last5 = fullname5.split(' ')
+            #passsenger5 = Passenger(first5, last5, Family, pref1)
+        #passenger1 = Passenger(first1, last2, Family, pref1)
+        #passenger2 = Passenger(first2, last2, Family, pref1)
+        #passenger3 Passenger(first3, last3, Family, pref1)
+    else:
+        messagebox.showwarning(title='Your Name', message='Please enter all of your first and last names.')
 
 def chooseCategory():
     category = clicked.get()
@@ -212,7 +250,7 @@ seatButtons(managerWindow, managerViewButtons, seatX, seatY)
 
 # -----------------------------------------------------------------------------------------------------------------
 # Creating and placing all buttons used in the GUI
-bookBusiness = Button(businessTicket, text='Book', bg='blue', fg='white', command=lambda: bookTicket()).place(x=740, y=770)
+bookBusiness = Button(businessTicket, text='Book', bg='blue', fg='white', command=lambda: bookBusinessTicket()).place(x=740, y=770)
 
 ticketButton = Button(passengerWindow, text='Book a seat', bg='blue', fg='white', command=lambda: bookSeat())
 ticketButton.place(x=5, y=770)
